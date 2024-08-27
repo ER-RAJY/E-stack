9faf5f8c844b436234b42e88ad2a8524e7878928
ER-RAJY <noureddineerajy@gmail.com>
2024-08-27 14:26:45 +0100

    add the path "/authentication" and "/signup" in the requestMatchers

---
+++ E_stack/src/test/java/com/example/E_stack/EStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

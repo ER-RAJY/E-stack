5aa796b424dab82e227740a7f9d6ce2ddcaa8529
ER-RAJY <noureddineerajy@gmail.com>
2024-09-05 04:02:53 +0100

    add methode login i  authService

--- E_stack_front/src/app/auth-services/auth-service/auth.service.ts
+++ E_stack_front/src/app/auth-services/auth-service/auth.service.ts
@@ -1,20 +1,39 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {map, Observable, tap} from 'rxjs';
+import {StorageService} from "../storage-service/storage.service";
 
 
 const BASIC_URL = 'http://localhost:8080/';
+export  const AUTH_HEADER = "autherization";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http : HttpClient){}
+  constructor(
+    private http : HttpClient,
+    private storage : StorageService
+  ){}
   sigunp(signupRequest:any):Observable<any>{
     return this.http.post(BASIC_URL+"signup",signupRequest)
   }
   login(loginRequest:any):Observable<any>{
-    return this.http.post(BASIC_URL+"authentication",loginRequest)
+    return this.http.post(BASIC_URL+"authentication",loginRequest,
+      {observe : "response" })
+      .pipe(
+        tap(__=>this.log("User Authentication")),
+        map((res:HttpResponse<any>)=>{
+          this.storage.saveUser(res.body);
+          const tokenLenght = res.headers.get(AUTH_HEADER)!.length;
+          const bearerToken = res.headers.get(AUTH_HEADER)!.substring(7, tokenLenght);
+          this.storage.saveToken(bearerToken);
+          return res;
+        })
+      )
+  }
+  log(message:String):void {
+    console.log("User auth Service "+message)
   }
 }

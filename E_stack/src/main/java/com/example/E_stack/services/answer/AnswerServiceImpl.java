ddae539b2a13a2365ba4deec7fae69b4c7083ffb
ER-RAJY <noureddineerajy@gmail.com>
2024-09-07 22:14:45 +0100

    update

--- E_stack/src/main/java/com/example/E_stack/services/answer/AnswerServiceImpl.java
+++ E_stack/src/main/java/com/example/E_stack/services/answer/AnswerServiceImpl.java
@@ -4,13 +4,13 @@ package com.example.E_stack.services.answer;
 import com.example.E_stack.dtos.AnswerDto;
 import com.example.E_stack.entities.Answer;
 import com.example.E_stack.entities.Question;
+import com.example.E_stack.entities.User;
 import com.example.E_stack.reposeitories.AnswerRepository;
 import com.example.E_stack.reposeitories.QuestionRepository;
 import com.example.E_stack.reposeitories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;

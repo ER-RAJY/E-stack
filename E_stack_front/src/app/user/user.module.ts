e2ccfbdc03e4cdbcd953dc625f41002464968b7f
ER-RAJY <noureddineerajy@gmail.com>
2024-09-19 23:43:04 +0100

    add static to methode hasToken() for use it in AuthGuard

--- E_stack_front/src/app/user/user.module.ts
+++ E_stack_front/src/app/user/user.module.ts
@@ -3,15 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostQuestionComponent } from './components/post-question/post-question.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatInputModule} from "@angular/material/input";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatIconModule} from "@angular/material/icon";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    PostQuestionComponent
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatChipsModule,
+    MatIconModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }

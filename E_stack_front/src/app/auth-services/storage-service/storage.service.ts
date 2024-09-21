fc6816e256eff3072cef32dd55a1538794a10244
ER-RAJY <noureddineerajy@gmail.com>
2024-09-21 14:35:16 +0100

    fix conflit

--- E_stack_front/src/app/auth-services/storage-service/storage.service.ts
+++ E_stack_front/src/app/auth-services/storage-service/storage.service.ts
@@ -8,7 +8,13 @@ const USER ='c_user';
 export class StorageService {
 
   constructor() { }
-
+  static hasToken(): boolean {
+    // Check if localStorage is defined before using it
+    if (typeof localStorage !== 'undefined' && localStorage.getItem(TOKEN) == null) {
+      return false;
+    }
+    return true;
+  }
   public saveUser(user:any){
     window.localStorage.removeItem(USER);
     window.localStorage.setItem(USER,JSON.stringify(user));
@@ -18,11 +24,11 @@ export class StorageService {
     window.localStorage.removeItem(TOKEN);
     window.localStorage.setItem(TOKEN,token);
   }
-   static getToken():string{
-      // @ts-ignore
-     return  localStorage.getItem(TOKEN);
-   }
-   static isUserLoggedIn(){
+  static getToken():string{
+    // @ts-ignore
+    return  localStorage.getItem(TOKEN);
+  }
+  static isUserLoggedIn(){
     if(this.getToken()== null ){
       return false;
     }

fc2789fd61ca8f16720dc8b4765427c3ee10c79d
ER-RAJY <noureddineerajy@gmail.com>
2024-09-29 03:49:01 +0100

    add service in angular for  view questions

--- E_stack_front/src/app/user/user.module.ts
+++ E_stack_front/src/app/user/user.module.ts
@@ -4,21 +4,22 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PostQuestionComponent } from './components/post-question/post-question.component';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatInputModule} from "@angular/material/input";
-import {MatChipsModule} from "@angular/material/chips";
-import {MatIconModule} from "@angular/material/icon";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatLegacyPaginatorModule} from "@angular/material/legacy-paginator";
-import {MatCardModule} from "@angular/material/card";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatInputModule } from "@angular/material/input";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatIconModule } from "@angular/material/icon";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatLegacyPaginatorModule } from "@angular/material/legacy-paginator";
+import { MatCardModule } from "@angular/material/card";
 import { ViewsQuestionComponent } from './components/views-question/views-question.component';
-
+import { GetQuestionsByUseridComponent } from './components/get-questions-by-userid/get-questions-by-userid.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     PostQuestionComponent,
-    ViewsQuestionComponent
+    ViewsQuestionComponent,
+    GetQuestionsByUseridComponent
   ],
   imports: [
     CommonModule,
